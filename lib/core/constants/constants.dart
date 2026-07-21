9d6ec9b21152de9db585d08aa4837b97bbb3e944
Yusuf Atak <yusufatak026@gmail.com>
2026-07-21 15:21:01 +0300

    UseCase eklendi ve bağımlılık yönetimi Riverpod ile sağlandı

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,4 @@
+const String newsAPIBaseURL = 'https://newsapi.org/v2';
+const String newsAPIKey = '258982adaa384351b9d306dc389ecb70'; // newsapi.org'dan ücretsiz alabilirsin
+const String countryQuery = 'us';
+const String categoryQuery = 'general';
---
+++ lib/features/daily_news/data/data_sources/remote/article_remote_data_source.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:news_app_clean_arch/features/daily_news/data/models/article.dart';
+
+abstract class ArticleRemoteDataSource {
+  Future<List<ArticleModel>> getNewsArticles();
+}
+
+class ArticleRemoteDataSourceImpl implements ArticleRemoteDataSource {
+  final http.Client client;
+  
+  ArticleRemoteDataSourceImpl({required this.client});
+
+  @override
+  Future<List<ArticleModel>> getNewsArticles() async {
+    final uri = Uri.parse('https://newsapi.org/v2/top-headlines?country=us&apiKey=258982adaa384351b9d306dc389ecb70');
+    final response = await client.get(uri);
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> jsonResponse = json.decode(response.body);
+      final List<dynamic> articlesJson = jsonResponse['articles'];
+
+      return articlesJson.map((json) => ArticleModel.fromJson(json)).toList();
+    } else {
+      throw Exception('API isteği başarısız oldu: ${response.statusCode}');
+    }
+
+  }
+}
